277daa90a2f1f19a075c4ba431e39740e5567c73
Gang5575 <60562483+Gang5575@users.noreply.github.com>
2020-02-02 16:30:30 +0700

    workshop1

---
+++ workhshop1/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        h1 > สวั สดี react < /h1> 
+        ทดสอบการทำงาน react js {
+            ""
+        } <
+        /div>
+    );
+}
+
+export default App;
